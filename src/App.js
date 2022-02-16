301793f1e06bc2c46795c6da076afd386b4b3efb
Dani <daniella.m.gagliardi@gmail.com>
2022-02-16 10:30:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 
-function App() {
+import img1 from "./assets/1.jpg"
+
+
+const App = () => {
+
+  // init Swiper:
+  const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+      <h1>Welcome to Guilt Free Shopping</h1>
+      <img src={img1} alt="Colorful Nike Air running shoes" />
+      <button>Start Shopping</button>
+      </section>
+      <section>
+        <form action="">
+          <label htmlFor="email" className='visuallyHidden'>Email Address:</label>
+          <input type="email" name="email" id="email" placeholder='Email' />
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" placeholder='Password' />
+          <input type="submit" value="SIGN IN" className='button'/>
+        </form>
+      </section>
     </div>
   );
 }
